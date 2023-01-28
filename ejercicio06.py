31e10f17dfed353f57ce55ecd7025706f29e21b6
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-27 19:04:36 -0500

    Terminado

--- ejercicio06.py
+++ ejercicio06.py
@@ -1,4 +1,10 @@
 '''Escribir una función que reemplace todas las apariciones
 de una subcadena en una cadena dada con otra subcadena
 dada.'''
-p
+
+def reemplazoSubcadedenas(cadeenaa, subcadedena, nuevaSubcadena):
+    return cadeenaa.replace(subcadedena, nuevaSubcadena)
+cadena = "Un guerrero nunca cede su poder ante nada, ni siquiera ante la muerte"
+print(reemplazoSubcadedenas(cadena, "guerrero", "Los guerreros"))
+# si la subcadena que se desea reemplazar no se encuentra en la cadena original,
+# la función devolverá la cadena original sin ningún cambio.
