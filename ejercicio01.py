46b3a87024d392ce1617b6aea9e5acb1fc5eeb8a
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-27 19:01:54 -0500

    Finish

--- ejercicio01.py
+++ ejercicio01.py
@@ -2,4 +2,14 @@
 palíndromo (se lee igual en ambos sentidos) o no:'''
 
 def palindromo():
-   pass
+    cadena =input("Introduzca  una frase u oracion que quiere determinar: ")
+    #convertamos la frase u oracion en minusculas, ya que en minusculas o mayusculas son iguales
+    cadena=cadena.lower()
+    #tambien debemos borrar los espaacios vacios que puedan estar sujetas a la oracion
+    cadena=cadena.replace(" ", "")
+    #evaluar si la cadena iantroducida es igual a su inversa
+    if cadena == cadena[::-1]:
+        return "La Cadena Introducida ES Palindromo..."
+    else:
+        return "la cadena Introducida NO es Palindromo... "
+print(palindromo())
