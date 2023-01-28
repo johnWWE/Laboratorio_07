3a278984057919b8a9630b04d214876f7e758a91
John Praddo <jhoncitof1ea10@gmail.com>
2023-01-27 19:04:10 -0500

    finish

--- ejercicio05.py
+++ ejercicio05.py
@@ -1,4 +1,24 @@
 ''' Escribir una función que determine la longitud de la
 subcadena más larga que contiene solo dígitos.'''
 def subcadenaLargaConDigitos():
-    pass
+    cadena = input("Ingresa la cadena : ")  # Pide la cadena al usuario
+    subcadenaLarga = ""  # variable para almacenar la subcadena más larga
+    subcadena = ""  # variable para almacenar la subcadena actual
+    # Recorremos cada carácter en la cadena
+    for caracter in cadena:
+        # Si el carácter es un dígito numérico, agrega al carácter a la subcadena actual
+        if caracter.isnumeric():  #metodo para ver si las subcadenas tienen digito o no
+            subcadena += caracter
+        # Si el carácter no es un dígito numérico,
+        else:
+            # si la subcadena actual es más larga que la subcadena más larga, actualiza la subcadena más larga
+            if len(subcadena) > len(subcadenaLarga):
+                subcadenaLarga = subcadena
+            # reinicia la subcadena actual
+            subcadena = ""
+    # Si la subcadena actual es más larga que la subcadena más larga, actualiza la subcadena más larga
+    if len(subcadena) > len(subcadenaLarga):
+        subcadenaLarga = subcadena
+    # Devuelve la longitud de la subcadena más larga
+    return len(subcadenaLarga)
+print(subcadenaLargaConDigitos())
